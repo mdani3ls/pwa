b104f026404f822bc59d503f4bfe27a3a91972ae
Daniel <dsm0ra3s@gmail.com>
2019-11-12 23:33:13 -0200

    alterando sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ self.addEventListener('install', function (event) {
     caches.open(CACHE_NAME).then(function (cache) {
       return cache.addAll([
         '/',
-        '/index.html',
+        '/pwa.html',
         '/manifest.js',
       ]);
     })
