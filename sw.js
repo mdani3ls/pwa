26c687b868b8ef0d4c73a7417073f3c15052a92d
Daniel <38056543+mdani3ls@users.noreply.github.com>
2024-07-11 12:53:18 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,8 @@ var filesToCache = [
   './',
   './index.html',
   './css/estilos.css',
-  './js/main.js'
+  './js/main.js',
+  './js/manifest.json'
 ];
 
 /* Start the service worker and cache all of the app's content */
